b4300ef49dbaa87127bc8aac02dad533111259f1
Almash N <almash050496@gmail.com>
2024-10-02 16:45:43 +0700

    remove empty folder

--- vite-project/src/pages/NotFound/NotFound.jsx
+++ vite-project/src/pages/NotFound/NotFound.jsx
@@ -1,10 +1,10 @@
-import * as S from "./notFound.styled"
+import * as S from "./notFound.styled";
 
 export const NotFound = () => {
   return (
     <S.Block404>
       <h1>Oops! Страница, которую вы ищете, не найдена</h1>
-      <img src="../../public/404.svg" alt="404-svg" />
+      <img src="/404.svg" alt="404-svg" />
     </S.Block404>
   );
 };
